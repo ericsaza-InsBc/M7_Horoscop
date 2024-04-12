890ec0cd25fd2f51777a5c5fe9ccdb1c632b389f
Borja Montseny <borja.montseny@gmail.com>
2024-04-12 20:35:26 +0200

    Comentaris al Model
    S'ha comentat el codi per fer-lo una mica més entenedor

--- app/Models/Horoscop.php
+++ app/Models/Horoscop.php
@@ -5,15 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Model d'un horòscop
 class Horoscop extends Model
 {
     use HasFactory;
 
+    // Aquí és on especifiquem els camps que es poden omplir
     protected $fillable = [
-        'date',
-        'lang',
-        'sign',
-        'time',
-        'phrase',
+        'date',     // Data
+        'lang',     // Llenguatge
+        'sign',     // Signe del zodíac
+        'time',     // Hora
+        'phrase',   // Frase segons signe de l'horòscop
     ];
 }
